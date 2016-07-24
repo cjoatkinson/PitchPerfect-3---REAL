fbc78ece7ec9b61d9e7420609f5424d15a04ebae
Thomas Atkinson <tom@mndisability.com>
2016-07-24 17:20:20 -0500

    Initial Commit

---
+++ PitchPerfect 3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PitchPerfect 3
+//
+//  Created by Christopher Atkinson on 7/24/16.
+//  Copyright © 2016 Christopher Atkinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
